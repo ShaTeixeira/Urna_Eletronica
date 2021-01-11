7a9245df20416ed5d5300358a57f3f19c6808925
Shayane Teixeira <shayaneteixeira56@gmail.com>
2021-01-11 17:15:51 -0300

    Adicionando arquivos e pastas
    Arquivos e pastas em JS, CSS e HTML

---
+++ Projeto_Urna_Eletronica/urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '25123',
+                nome: 'Amarildo Cunha',
+                partido: 'PCdoB',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '13678',
+                nome: 'Maria Santiago',
+                partido: 'PT',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Leonardo Carvalho',
+                partido: 'PDT',
+                vice: 'José Valetim',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '18',
+                nome: 'Fernando Cunha',
+                partido: 'PSOL',
+                vice: 'Ana Alves',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
